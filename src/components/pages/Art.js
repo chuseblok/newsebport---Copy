8c8a146db25376ad47286b012930e2e43785f178
sebastian Chunley <sebastian.chunley@gmail.com>
2022-10-27 15:43:56 -0400

    art cards

--- src/components/pages/Art.js
+++ src/components/pages/Art.js
@@ -3,17 +3,22 @@ import React from "react";
 export default function Art() {
   return (
     <div class='C3'>
-      <div class='card2'>
-        <div class='BD'></div>
-        <div class='BP2'>
-          <a
-            class='buttonBD'
-            href='https://www.instagram.com/sebastianchumley_chuseblok/'
-          >
-            Coming soon, but feel free to checkout my instagram
-          </a>
-        </div>
-      </div>
+      <button class='BD'></button>
+      <button class='MM'></button>
+      <button class='FM'></button>
+      <button class='AG'></button>
+      <button class='SL'></button>
+      <button class='SW'></button>
+      <button class='AS'></button>
+      <button class='FD'></button>
+      <button class='FR'></button>
+      <button class='HZ'></button>
+      <button class='RS'></button>
+      <button class='SH'></button>
+      <button class='SU'></button>
+      <button class='BF'></button>
+      <button class='CH'></button>
+      <button class='GR'></button>
     </div>
   );
 }
