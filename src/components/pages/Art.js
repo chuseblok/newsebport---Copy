2fd558031574c337f5c2d17cbd21342e38258a30
sebastian Chunley <sebastian.chunley@gmail.com>
2022-10-28 14:29:32 -0400

    getting rid of picture focus

--- src/components/pages/Art.js
+++ src/components/pages/Art.js
@@ -3,30 +3,29 @@ import React from "react";
 export default function Art() {
   return (
     <div class='C3'>
-      <div class='column'>
+      <div class='set'>
         <button class='BD'></button>
         <button class='MM'></button>
         <button class='FM'></button>
-      </div>
-      <div class='column'>
         <button class='HZ'></button>
         <button class='SL'></button>
         <button class='SW'></button>
       </div>
-      <div class='column'>
+      <div class='set'>
         <button class='AS'></button>
         <button class='FD'></button>
         <button class='FR'></button>
-      </div>
-      <div class='column'>
         <button class='RS'></button>
         <button class='SH'></button>
         <button class='SU'></button>
       </div>
-      <div class='column'>
+      <div class='set'>
         <button class='BF'></button>
         <button class='CH'></button>
         <button class='GR'></button>
+        <button class='AG'></button>
+        <button class='SN'></button>
+        <button class='WW'></button>
       </div>
     </div>
   );
