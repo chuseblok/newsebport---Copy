4ef4cf37735a8a257d5b4b41e0a2f8d94fb6689f
sebastian Chunley <sebastian.chunley@gmail.com>
2022-10-28 10:19:02 -0400

    arranged cards

--- src/components/pages/Art.js
+++ src/components/pages/Art.js
@@ -3,22 +3,31 @@ import React from "react";
 export default function Art() {
   return (
     <div class='C3'>
-      <button class='BD'></button>
-      <button class='MM'></button>
-      <button class='FM'></button>
-      <button class='AG'></button>
-      <button class='SL'></button>
-      <button class='SW'></button>
-      <button class='AS'></button>
-      <button class='FD'></button>
-      <button class='FR'></button>
-      <button class='HZ'></button>
-      <button class='RS'></button>
-      <button class='SH'></button>
-      <button class='SU'></button>
-      <button class='BF'></button>
-      <button class='CH'></button>
-      <button class='GR'></button>
+      <div class='column'>
+        <button class='BD'></button>
+        <button class='MM'></button>
+        <button class='FM'></button>
+      </div>
+      <div class='column'>
+        <button class='HZ'></button>
+        <button class='SL'></button>
+        <button class='SW'></button>
+      </div>
+      <div class='column'>
+        <button class='AS'></button>
+        <button class='FD'></button>
+        <button class='FR'></button>
+      </div>
+      <div class='column'>
+        <button class='RS'></button>
+        <button class='SH'></button>
+        <button class='SU'></button>
+      </div>
+      <div class='column'>
+        <button class='BF'></button>
+        <button class='CH'></button>
+        <button class='GR'></button>
+      </div>
     </div>
   );
 }
